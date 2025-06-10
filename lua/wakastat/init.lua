263fcaee842e889ab7c715df2bd73db61d622b8d
Fiqry choerudin <fiqrychoerudin48@gmail.com>
2025-06-10 18:41:02 +0700

    fix: bugs text not showing

--- lua/wakastat/init.lua
+++ lua/wakastat/init.lua
@@ -1,5 +1,4 @@
 local M = {}
-
 local uv = vim.uv
 
 ---@class WakastatConfig
@@ -17,37 +16,59 @@ local config = {
 
 local wakatime_cache = "WakaTime: N/A"
 local last_update = 0
+local updating = false
 
 ---@return nil
 local function update_wakatime()
+	-- Prevent multiple simultaneous updates
+	if updating then
+		return
+	end
+	updating = true
+
 	local stdout = uv.new_pipe(false)
 	if not stdout then
+		updating = false
 		return
 	end
 
 	local handle = uv.spawn("wakatime-cli", {
 		args = config.args,
 		stdio = { nil, stdout, nil },
-	}, function()
+	}, function(code, signal)
+		updating = false
 		if handle then
 			handle:close()
 		end
-		stdout:close()
+		if stdout and not stdout:is_closing() then
+			stdout:close()
+		end
+		-- Update timestamp even if command failed
+		last_update = os.time()
 	end)
 
 	if not handle then
 		stdout:close()
+		updating = false
 		return
 	end
 
 	local data = {}
 	stdout:read_start(function(err, chunk)
-		assert(not err, err)
+		if err then
+			wakatime_cache = "WakaTime: Error"
+			return
+		end
+
 		if chunk then
 			table.insert(data, chunk)
 		else
-			wakatime_cache = config.format:format(table.concat(data):gsub("\n", "  "))
-			last_update = os.time()
+			local result = table.concat(data):gsub("\n", ""):gsub("^%s*(.-)%s*$", "%1")
+			if result and result ~= "" then
+				wakatime_cache = config.format:format(result)
+			else
+				wakatime_cache = "WakaTime: No data"
+			end
 		end
 	end)
 end
@@ -56,6 +77,8 @@ end
 ---@return nil
 function M.setup(user_config)
 	config = vim.tbl_deep_extend("force", config, user_config or {})
+	-- Initial update
+	update_wakatime()
 end
 
 ---@return string
@@ -71,13 +94,16 @@ end
 ---@return table
 function M.add_to_heirline(existing_statusline)
 	local wakatime_component = {
-		provider = M.wakatime,
+		provider = function()
+			return " " .. M.wakatime() .. " "
+		end,
 		hl = { fg = "cyan", bg = "NONE" },
 	}
 
 	-- Insert WakaTime component at the end of existing statusline
 	local new_statusline = vim.tbl_deep_extend("force", {}, existing_statusline)
 	table.insert(new_statusline, wakatime_component)
+
 	return new_statusline
 end
 
