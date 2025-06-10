da7bdcda7b1e74c914b7c1073d5eead6ea60bdaa
Fiqry choerudin <fiqrychoerudin48@gmail.com>
2025-06-10 18:23:29 +0700

    fix: wakatime replacing other items

--- lua/wakastat/init.lua
+++ lua/wakastat/init.lua
@@ -1,8 +1,14 @@
 local M = {}
 
-local uv = vim.loop
+local uv = vim.uv
+
+---@class WakastatConfig
+---@field args string[]
+---@field format string
+---@field update_interval number
 
 -- Default config
+---@type WakastatConfig
 local config = {
 	args = { "--today" },
 	format = "Today Coding Time: %s",
@@ -12,21 +18,29 @@ local config = {
 local wakatime_cache = "WakaTime: N/A"
 local last_update = 0
 
+---@return nil
 local function update_wakatime()
-	---@diagnostic disable-next-line: undefined-field
 	local stdout = uv.new_pipe(false)
-	local handle
-	---@diagnostic disable-next-line: undefined-field
-	handle = uv.spawn("wakatime-cli", {
+	if not stdout then
+		return
+	end
+
+	local handle = uv.spawn("wakatime-cli", {
 		args = config.args,
 		stdio = { nil, stdout, nil },
 	}, function()
-		handle:close()
+		if handle then
+			handle:close()
+		end
 		stdout:close()
 	end)
 
+	if not handle then
+		stdout:close()
+		return
+	end
+
 	local data = {}
-	---@diagnostic disable-next-line: undefined-field
 	stdout:read_start(function(err, chunk)
 		assert(not err, err)
 		if chunk then
@@ -38,15 +52,33 @@ local function update_wakatime()
 	end)
 end
 
+---@param user_config? WakastatConfig
+---@return nil
 function M.setup(user_config)
 	config = vim.tbl_deep_extend("force", config, user_config or {})
 end
 
+---@return string
 function M.wakatime()
 	if os.time() - last_update > config.update_interval then
 		update_wakatime()
 	end
 	return wakatime_cache
+end
+
+-- Function to add WakaTime component to existing heirline statusline
+---@param existing_statusline table
+---@return table
+function M.add_to_heirline(existing_statusline)
+	local wakatime_component = {
+		provider = M.wakatime,
+		hl = { fg = "cyan", bg = "NONE" },
+	}
+
+	-- Insert WakaTime component at the end of existing statusline
+	local new_statusline = vim.tbl_deep_extend("force", {}, existing_statusline)
+	table.insert(new_statusline, wakatime_component)
+	return new_statusline
 end
 
 return M
